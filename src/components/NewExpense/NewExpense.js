5b1cc02de79935bb8ac5a18de5a8fbb03a9e042b
Uddinrazi <uddinrazi88@gmail.com>
2024-02-08 09:30:55 +0530

    using multiple usestate

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ExpenseForm from "./ExpenseForm";
+import "./NewExpense.css";
 
 const NewExpense = () => {
   return (
-    <div className="NewExpense">
+    <div className="new-expense">
       <ExpenseForm />
     </div>
   );
